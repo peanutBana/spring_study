fcfbb8615205503ba8da2905fcb14e4d409a89a5
peanutBana <diao0321@gmail.com>
2023-01-11 17:38:10 +0900

    'thymeleaf'

---
+++ myshop/src/main/java/com/myshop/controller/ThymeleafExController.java
@@ -0,0 +1,18 @@
+package com.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller		//컨트롤러의 역할을하는 클래스를 정의
+@RequestMapping(value="/thymeleaf")		//url 경로 지정
+public class ThymeleafExController {
+	
+	@GetMapping(value="/ex01")
+	public String thymeleafEx01(Model model) {
+		model.addAttribute("date", "타임리프 예제 입니다.");
+		
+		return null;
+	}
+}
