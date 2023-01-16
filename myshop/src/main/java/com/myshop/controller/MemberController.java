0e9cdd298a58d43ccfbaec5af8ad42a3b1feb149
peanutBana <diao0321@gmail.com>
2023-01-16 16:56:30 +0900

    login

--- myshop/src/main/java/com/myshop/controller/MemberController.java
+++ myshop/src/main/java/com/myshop/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.myshop.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,13 +34,25 @@ public class MemberController {
    //회원가입 버튼을 눌렀을 때 실행되는 메소드
    //post방식 request
    @PostMapping(value = "/new")
-   public String memberForm(MemberFormDto memberFormDto) {
-      Member member = Member.createMember(memberFormDto, passwordEncoder);
-      memberService.saveMember(member);
+   public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+	   //@Valid : 유혀성을 검증하려는 객체 앞에 붙인다.
+	   //BindingResult : 유효성 검증 후에 결과를 넣어준다.
+	   
+	   if(bindingResult.hasErrors()) {
+		   return "member/memberFrom";
+	   }
+	   
+      try {
+    	  Member member = Member.createMember(memberFormDto, passwordEncoder);
+          memberService.saveMember(member);
+      }catch(IllegalStateException e) {
+    	  model.addAttribute("errorMessage",e.getMessage());
+    	  return "member/memberFrom";
+      }
       
       return "redirect: /";
       //redirect를 쓰는 이유는
-      //1.URL의 변화여부가 필요할 때.
+      //1. URL의 변화여부가 필요할 때.
       //2. 다음 새로운 요청 url을 보낼 때 최초요청 url은 유효하지 않고 이 두 요청은 전혀 다른 요청을 수행한다.
       //3. 시스템에 변화가 생기는 요청(로그인, 회원가입, 글쓰기)의 경우
    }
