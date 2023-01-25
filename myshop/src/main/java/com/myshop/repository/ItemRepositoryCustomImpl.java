1cde64aa336e197b60df7fdd58fdfa030de22655
peanutBana <diao0321@gmail.com>
2023-01-25 14:24:04 +0900

    mainItem

--- myshop/src/main/java/com/myshop/repository/ItemRepositoryCustomImpl.java
+++ myshop/src/main/java/com/myshop/repository/ItemRepositoryCustomImpl.java
@@ -13,8 +13,10 @@ import org.thymeleaf.util.StringUtils;
 import com.myshop.constant.ItemSellStatus;
 import com.myshop.dto.ItemSearchDto;
 import com.myshop.dto.MainItemDto;
+import com.myshop.dto.QMainItemDto;
 import com.myshop.entity.Item;
 import com.myshop.entity.QItem;
+import com.myshop.entity.QItemImg;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Wildcard;
@@ -82,10 +84,41 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 		
 		return new PageImpl<>(content, pageable, total);
 	}
+	
+	private BooleanExpression itemNmLike(String searchQuery) {
+		return StringUtils.isEmpty(searchQuery) ? null : QItem.item.itemNm.like("%" + searchQuery + "%");
+	}
 
 	@Override
 	public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+		QItem item = QItem.item;
+		QItemImg itemImg = QItemImg.itemImg;
 		
-		return null;
+		List<MainItemDto> content = queryFactory.select(
+					new QMainItemDto(
+							item.id, 
+							item.itemNm, 
+							item.itemDetail, 
+							itemImg.imgUrl, 
+							item.price
+							)
+					).from(itemImg)
+				     .join(itemImg.item, item)
+				     .where(itemImg.repimgYn.eq("Y"))
+				     .where(itemNmLike(itemSearchDto.getSearchQuery()))
+				     .orderBy(item.id.desc())
+				     .offset(pageable.getOffset())
+					 .limit(pageable.getPageSize())
+					 .fetch();
+		
+		//count(*)
+		Long total = queryFactory.select(Wildcard.count)
+					 .from(itemImg)
+				     .join(itemImg.item, item)
+				     .where(itemImg.repimgYn.eq("Y"))
+				     .where(itemNmLike(itemSearchDto.getSearchQuery()))
+				     .fetchOne();
+		
+		return new PageImpl<>(content, pageable, total);
 	}
 }
