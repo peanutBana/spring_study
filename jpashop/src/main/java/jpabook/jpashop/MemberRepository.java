5255967a7f48c3e232b2ea5075dc15dddb5aacf2
peanutBana <diao0321@gmail.com>
2023-12-03 14:11:23 +0900

    test code

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.Member;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext //entity manager 주입
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();  //id 정도만 조회
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,17 +1,14 @@
 package jpabook.jpashop;
 
-import jpabook.Member;
-import jpabook.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.statements.SpringRepeat;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
@@ -20,8 +17,10 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @Rollback(false)
     @Transactional
-    public void testMember() throws Exception{
+    public void testMember() throws Exception {
+
         //given
         Member member = new Member();
         member.setUsername("memberA");
@@ -33,6 +32,7 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
     }
 }
